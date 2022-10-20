07f60c21872459b1eb83f26ac8414c110e7fa357
xiaosagecy <84890928+xiaosagecy@users.noreply.github.com>
2022-10-20 19:38:06 +0800

    login init

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   // import.meta.env.BASE_URL是vite配置的路由基准地址，默认是'/'
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    { path: '/login', component: () => import('@/views/Login/index.vue') }
+  ]
 })
 
 export default router
---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="login-page">
+        <van-button type="primary">按钮</van-button>
+        <van-checkbook :check="true">复选框</van-checkbook>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { Button as VanButton, Checkbox as VanCheckbox } from 'vant'
+</script>
+
+<style scoped lang="scss">
+
+</style>
