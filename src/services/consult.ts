4d67eba08c37b20958e9f012ef25844b1e248e4a
潇洒哥 <871543255@qq.com>
2022-10-25 19:26:41 +0800

    关注医生展示列表

--- src/services/consult.ts
+++ src/services/consult.ts
@@ -1,8 +1,15 @@
-import type { KnowledgePage, KnowledgeParams } from '@/types/consult'
+import type { KnowledgePage, KnowledgeParams, DoctorPage, PageParams } from '@/types/consult'
 import { request } from '@/utils/request'
 
 
+// 获取知识列表信息
 export const getKonwledgePage = (params: KnowledgeParams) =>
     request<KnowledgePage>('/patient/home/knowledge', 'get', params)
+
+
+// 获取推荐关注医生
+export const getDoctorPage = (params: PageParams) =>
+    request<DoctorPage>('/home/page/doc', 'get', params)
+
 
 
--- src/views/Home/components/FollowDoctor.vue
+++ src/views/Home/components/FollowDoctor.vue
@@ -9,8 +9,8 @@
             <!-- 去除指示器:show-indicators="false"，关闭无缝滚动，设置一次滚动一个卡片 loop是否开启循环播放-->
             <!-- :width适配不同设备 -->
             <van-swipe :width="(150 / 375) * width" :show-indicators="false" :loop="false">
-                <van-swipe-item v-for="item in 5" :key="item">
-                    <DoctorCard />
+                <van-swipe-item v-for="item in list" :key="item.id">
+                    <DoctorCard :item="item" />
                 </van-swipe-item>
             </van-swipe>
         </div>
@@ -19,10 +19,20 @@
 
 <script setup lang='ts'>
 import DoctorCard from './DoctorCard.vue'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useWindowSize } from '@vueuse/core'
+import { getDoctorPage } from '@/services/consult'
+import type { DoctorList } from '@/types/consult'
 
 const { width } = useWindowSize()
+const list = ref<DoctorList>()
+
+const loadData = async () => {
+    const res = await getDoctorPage({ current: 1, pageSize: 5 })
+    list.value = res.data.rows
+}
+
+onMounted(() => loadData())
 
 
 
