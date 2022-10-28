70ae65bf3a8792af8a6a4087350d949445cd55dd
潇洒哥 <871543255@qq.com>
2022-10-28 19:34:39 +0800

    病情描述基础布局01

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,8 @@ const router = createRouter({
         { path: '/user', component: () => import('@/views/User/index.vue'), meta: { title: '个人中心' } },
         { path: '/user/patients', component: () => import('@/views/User/PatientPage.vue'), meta: { title: '家庭档案' } },
         { path: '/consult/fast', component: () => import('@/views/Consult/ConsultFast.vue'), meta: { title: '极速问诊' } },
-        { path: '/consult/dep', component: () => import('@/views/Consult/ConsultDep.vue'), meta: { title: '选择科室' } }
+        { path: '/consult/dep', component: () => import('@/views/Consult/ConsultDep.vue'), meta: { title: '选择科室' } },
+        { path: '/consult/illness', component: () => import('@/views/Consult/ConsultIllness.vue'), meta: { title: '病情描述' } }
 
       ]
     }
---
+++ src/views/Consult/ConsultIllness.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="consult-illness-page">
+        <cp-nav-bar title="图文问诊" />
+        <!-- 医生提示 -->
+        <div class="illness-tip" van-hairline--bottom>
+            <!-- img是行内元素 独占一行 -->
+            <img src="@/assets/avatar-doctor.svg" alt="">
+            <div class="info">
+                <p class="tit">在线医生</p>
+                <p class="tip">请描述你的疾病或症状、是否用药、就诊经历，需要我听过什么样的帮助</p>
+                <p class="safe">
+                    <cp-icon name="consult-safe" />
+                    <span>内容权医生可见</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang='ts'>
+
+</script>
+
+<style scoped lang="scss">
+.consult-illness-page {
+    padding-top: 46px;
+}
+
+.illness-tip {
+    display: flex;
+    padding: 15px;
+
+    .img {
+        width: 52px;
+        height: 52px;
+        border-radius: 4px;
+        overflow: hidden;
+        margin-top: 10px;
+    }
+
+    .info {
+        flex: 1;
+        padding-left: 12px;
+
+        .tit {
+            font-size: 16px;
+            margin-bottom: 5px;
+        }
+
+        .tip {
+            padding: 12px;
+            background: var(--cp-bg);
+            color: var(--cp-text3);
+            font-size: 13px;
+            margin-bottom: 10px;
+        }
+
+        .safe {
+            font-size: 10px;
+            color: var(--cp-text3);
+            display: flex;
+            align-items: center;
+
+            .cp-icon {
+                font-size: 12px;
+                margin-right: 2px;
+            }
+        }
+    }
+}
+</style>
+
