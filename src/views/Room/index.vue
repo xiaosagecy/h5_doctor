d33381b2e1021d4f99544bce8fbb7a14c04d3587
潇洒哥 <871543255@qq.com>
2022-10-31 19:40:20 +0800

    问诊室-消息卡片

---
+++ src/views/Room/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="room-page">
+        <cp-nav-bar title="问诊室" />
+        <room-status></room-status>
+        <room-message></room-message>
+        <room-action></room-action>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import RoomStatus from './components/RoomStatus.vue'
+import RoomMessage from './components/RoomMessage.vue'
+import RoomAction from './components/RoomAction.vue'
+
+</script>
+
+<style scoped lang="scss">
+.room-page {
+    padding-top: 90px;
+    padding-bottom: 60px;
+    min-height: 100vh;
+    box-sizing: border-box;
+    background-color: var(--cp-bg);
+
+    .van-pull-refresh {
+        width: 100%;
+        min-height: calc(100vh - 150px);
+    }
+}
+</style>
