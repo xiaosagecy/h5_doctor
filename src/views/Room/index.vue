ec50fc4b0865a83bd67ecbfb8cf9386892875c19
潇洒哥 <871543255@qq.com>
2022-11-01 21:15:31 +0800

    建立数据类型，测试socket.io连接

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -11,6 +11,51 @@
 import RoomStatus from './components/RoomStatus.vue'
 import RoomMessage from './components/RoomMessage.vue'
 import RoomAction from './components/RoomAction.vue'
+import type { Socket } from 'socket.io-client'
+import { io } from 'socket.io-client'
+import { onMounted, onUnmounted } from 'vue'
+import { baseURL } from '@/utils/request'
+import { useUserStore } from '@/stores'
+import { useRoute } from 'vue-router'
+
+const store = useUserStore()
+const route = useRoute()
+
+let socket: Socket
+
+onUnmounted(() => {
+    socket.close()
+})
+
+onMounted(() => {
+    // 建立连接，创建 socket.io 实例
+    socket = io(baseURL, {
+        // 身份认证
+        auth: {
+            token: `Bearer ${store.user?.token}`
+        },
+        // 订单ID
+        query: {
+            // 地址栏取出orderId，在建立连接的时候发送给，socket服务器
+            orderId: route.query.orderId
+        }
+    })
+
+    socket.on('connect', () => {
+        // 建立连接成功
+        console.log('connect')
+    })
+
+    socket.on('error', (event) => {
+        // 错误异常消息
+        console.log('error')
+    })
+
+    socket.on('disconnect', () => {
+        // 已经断开链接
+        console.log('disconnect')
+    })
+})
 
 </script>
 
